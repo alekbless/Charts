fb7ba6c36ae1356886ac2bcb190467cd4727bb69
alekbless <aleksander@sbless.no>
2017-11-07 22:15:29 +0100

    Changed multichart to my needs

--- resources/views/highcharts/multi/bar.blade.php
+++ resources/views/highcharts/multi/bar.blade.php
@@ -9,6 +9,12 @@
                 plotShadow: false,
                 type: 'column'
             },
+            tooltip: {
+                formatter: function() {
+                    var tooltip = Highcharts.dateFormat('%d.%m.%Y - %H:%M:%S', this.x) + ' : ' + Highcharts.numberFormat((this.y),2,'.');
+                    return tooltip;
+                }
+            },
             @if($model->title)
                 title: {
                     text:  "{!! $model->title !!}"
@@ -25,18 +31,43 @@
                    borderWidth: 0
                }
            },
-           xAxis: {
+           /*xAxis: {
                 categories: [
                     @foreach($model->labels as $label)
                          "{!! $label !!}",
                     @endforeach
                 ],
+            },*/
+            xAxis: {
+                type: 'datetime',
+                title: {
+                    text: "{!! $model->x_axis_title !!}"
+                },
             },
+            /*
             yAxis: {
                 title: {
                     text: "{!! $model->y_axis_title === null ? $model->element_label : $model->y_axis_title !!}"
                 },
-            },
+            },*/
+
+            yAxis: [{ // Primary yAxis
+             labels: {
+                 format: '{value} kWh',
+             },
+             title: {
+                 text: 'kWh',
+             }
+            }, { // Secondary yAxis
+             title: {
+                 text: 'Temperatur',
+             },
+             labels: {
+                 format: '{value} °C',
+
+             },
+             opposite: true
+            }],
             legend: {
                 @if(!$model->legend)
                     enabled: false,
@@ -45,6 +76,7 @@
             series: [
                 @for ($i = 0; $i < count($model->datasets); $i++)
                     {
+                        type: '{!! $model->datasets[$i]['type'] !!}',
                         name:  "{!! $model->datasets[$i]['label'] !!}",
                         @if($model->colors && count($model->colors) > $i)
                             color: "{{ $model->colors[$i] }}",
